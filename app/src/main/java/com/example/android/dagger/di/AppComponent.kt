c8e419eb5808dcdb798fa71cc373c088d68a35bd
Norio Egi <norio.egi@agoop.co.jp>
2020-04-15 16:44:21 +0900

    Reafctored several classes

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,31 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        StorageModule::class,
+        AppSubComponents::class
+    ]
+)
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun registrationComponent(): RegistrationComponent.Factory
+
+    fun loginComponent(): LoginComponent.Factory
+
+    //inject
+    fun inject(activity: MainActivity)
+}
